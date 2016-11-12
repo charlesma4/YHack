1e8d2adee28eecebd3bd1513513b29944712ae97
charlesma4 <charlesma4@gmail.com>
2016-11-12 00:44:05 -0500

    just added standard scanner kinda thing, and now needa use variable edit_message with the api in order to retrieve bank info

--- app/src/main/java/com/example/charl/yhackapp_financialmanagement/MainActivity.java
+++ app/src/main/java/com/example/charl/yhackapp_financialmanagement/MainActivity.java
@@ -2,12 +2,17 @@ package com.example.charl.yhackapp_financialmanagement;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.EditText;
+import java.util.*;
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void inputAccountNumber(View view) {
+        EditText accountNumber = (EditText) findViewById(R.id.edit_message);
     }
 }
