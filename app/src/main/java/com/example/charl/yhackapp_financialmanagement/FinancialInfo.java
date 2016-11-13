32e61137038154d44cd9fd60961b96af9f3bcd30
charlesma4 <charlesma4@gmail.com>
2016-11-13 04:34:38 -0500

    tried to do it

--- app/src/main/java/com/example/charl/yhackapp_financialmanagement/FinancialInfo.java
+++ app/src/main/java/com/example/charl/yhackapp_financialmanagement/FinancialInfo.java
@@ -1,8 +1,12 @@
 package com.example.charl.yhackapp_financialmanagement;
 
+import android.view.ViewGroup;
+import android.widget.TextView;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import android.support.v7.widget.Toolbar;
 
 public class FinancialInfo extends AppCompatActivity {
 
@@ -12,15 +16,48 @@ public class FinancialInfo extends AppCompatActivity {
         setContentView(R.layout.activity_financial_info);
 
         Intent intent = getIntent();
-        int currentAssets = intent.getIntExtra(MainActivity.EXTRA_MESSAGE,MainActivity.money);
-        int time = getEstimatedTime(currentAssets);
+        int yourMoney = (intent.getIntExtra(MainActivity.EXTRA_MESSAGE1, 0));
+        int collegeExpenses = (intent.getIntExtra(MainActivity.EXTRA_MESSAGE2,0));
+        int otherExpenses = (intent.getIntExtra(MainActivity.EXTRA_MESSAGE3,0));
+        int income = (intent.getIntExtra(MainActivity.EXTRA_MESSAGE4,0));
+        double daysLeft = estimateTimeLeft(yourMoney,collegeExpenses,otherExpenses, income);
+        if(income/14>((collegeExpenses+otherExpenses)/7)) {
+            String message = "Your assets will increase to "+daysLeft+" after a month. Keep it up!";
+            TextView textView = new TextView(this);
+            textView.setTextSize(40);
+            textView.setText(message);
+            ViewGroup layout = (ViewGroup) findViewById(R.id.activity_financial_info);
+            layout.addView(textView);
+        }
+        else {
+            String message = "Your current situation will last "+daysLeft+" days.";
+            TextView textView = new TextView(this);
+            textView.setTextSize(40);
+            textView.setText(message);
+            ViewGroup layout = (ViewGroup) findViewById(R.id.activity_financial_info);
+            layout.addView(textView);
+        }
     }
 
-    public int getEstimatedTime(int money) {
-
+    public static double estimateTimeLeft(int assets, int cexpenses, int oexpenses, int income) {
+        if(income/14>(cexpenses+oexpenses)/7) {
+            return assets + (income/14)*30 - ((cexpenses+oexpenses)/7)*30;
+        }
+        double initial = assets/((cexpenses+oexpenses)/7);
+        double difference=11;
+        while(difference>10) {
+            double previousAssets = assets;
+            double incomeMade = initial * (income / 14);
+            assets += incomeMade;
+            difference = assets-previousAssets;
+            initial = difference;
+        }
+        return assets/((cexpenses+oexpenses)/7);
     }
-
-
-
 
 }
+
+
+
+
+
--- app/src/main/java/com/example/charl/yhackapp_financialmanagement/MainActivity.java
+++ app/src/main/java/com/example/charl/yhackapp_financialmanagement/MainActivity.java
@@ -5,31 +5,39 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
 import java.util.*;
+
+import static android.R.attr.button;
+
 public class MainActivity extends AppCompatActivity {
-    public static final String EXTRA_MESSAGE = "com.example.YHack.MESSAGE";
-    public static int money;
-    @Override
+    public static final String EXTRA_MESSAGE1 = "com.example.YHack.MESSAGE1";
+    public static final String EXTRA_MESSAGE2 = "com.example.YHack.MESSAGE2";
+    public static final String EXTRA_MESSAGE3 = "com.example.YHack.MESSAGE3";
+    public static final String EXTRA_MESSAGE4 = "com.example.YHack.MESSAGE4";
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    public void sendAssets(View view) {
-        Intent intent = new Intent(this, FinancialInfo.class);
-        EditText input = (EditText) findViewById(R.id.editAssets);
-        int totalMoney = Integer.parseInt(input.getText().toString());
-        money=totalMoney;
-        intent.putExtra(EXTRA_MESSAGE,totalMoney);
+    public void sendFinancialInfo(View view) {
+        Intent intent = new Intent(MainActivity.this, FinancialInfo.class);
+
+        EditText input1 = (EditText) findViewById(R.id.editAssets);
+        EditText input2 = (EditText) findViewById(R.id.editCollegeExpenses);
+        EditText input3 = (EditText) findViewById(R.id.editOtherExpenses);
+        EditText input4 = (EditText) findViewById(R.id.editIncome);
+        int yourMoney = Integer.parseInt(input1.getText().toString());
+        int collegeExpenses = Integer.parseInt(input2.getText().toString());
+        int otherExpenses = Integer.parseInt(input3.getText().toString());
+        int income = Integer.parseInt(input4.getText().toString());
+        intent.putExtra(EXTRA_MESSAGE1, yourMoney);
+        intent.putExtra(EXTRA_MESSAGE2, collegeExpenses);
+        intent.putExtra(EXTRA_MESSAGE3, otherExpenses);
+        intent.putExtra(EXTRA_MESSAGE4, income);
         startActivity(intent);
     }
-    public void sendFoodExpenses(View view) {
 
-    }
-    public void sendGeneralCollegeExpenses(View view) {
-
-    }
-    public void sendOtherExpenses(View view) {
-
-    }
 }
